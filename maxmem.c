f5b0c1461099e631735edd39ddd76bae59d29dd9
allexkap <aekpstn@gmail.com>
2023-06-17 23:39:25 +0300

    add parse rss

--- maxmem.c
+++ maxmem.c
@@ -3,6 +3,21 @@
 #include <unistd.h>
 
 
+size_t collect(pid_t pid) {
+    static char cmd[32] = {};
+    static FILE *pipe = NULL;
+
+    snprintf(cmd, 32, "ps -horss -p%d", pid);
+    pipe = popen(cmd, "r");
+
+    size_t size = 0;
+    fscanf(pipe, "%d", &size);
+    pclose(pipe);
+
+    return size;
+}
+
+
 int main(int argc, char *argv[]) {
 
     if (argc < 2) {
@@ -17,6 +32,15 @@ int main(int argc, char *argv[]) {
     }
 
     printf("pid %d\n", pid);
+
+
+    int size = 0, res = 0;
+    for (int i = 0; i < 100; ++i) {
+        res = collect(pid);
+        if (res > size) size = res;
+    }
+
+    printf("%d kB\n", res);
 
     return 0;
 }
